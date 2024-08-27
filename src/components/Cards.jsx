52f74c36747a4d1b3e7510e74e9f1013c89cf5e2
Tyler <brown.tyler.w@gmail.com>
2024-08-26 23:41:36 -0700

    Game accurately tracks scores and keeps track of which pokemon have already been clicked

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -3,11 +3,14 @@ import { useState, useEffect } from "react";
 function Cards() {
     const [currentScore, setCurrentScore] = useState(0)
     const [bestScore, setBestScore] = useState(0)
+    // Array used to check if a pokemon has been clicked or not
+    const [selectedCard, setSelectedCard] = useState([])
     const [data, setData] = useState([])
 
     useEffect(() => {
         async function fetchPokemon() {
             try {
+                // Grabs info for only Gens 1-3 (the best ones)
                 const res = await fetch('https://pokeapi.co/api/v2/pokemon/?limit=386');
                 const data = await res.json();
                 const cleanedData = await Promise.all(
@@ -26,6 +29,28 @@ function Cards() {
         fetchPokemon();
     }, [])
 
+    // Updates bestScore only if the next updated currentScore is greater
+    const checkBestScore = (score) => {
+        setBestScore(
+            score > bestScore ? score : bestScore
+        );
+    }
+
+    // Pushes pokemon name to selectedCard if that pokemon does not exist in it
+    // If pokemon name already exists, clears array and resets currentScore
+    // If name does not exist, scores are updated accordingly
+    const handlePokemonClick = (name) => {
+        if (selectedCard.includes(name)) {
+            setCurrentScore(0);
+            setSelectedCard([]);
+        } else {
+            setCurrentScore(currentScore+1);
+            checkBestScore(currentScore+1);
+            setSelectedCard(selectedCard => [...selectedCard, name]);
+        }
+    }
+
+    // Grabs 12 random pokemon from the 386 and creates a grid of divs for each one
     const createGrid = () => {
         const grid = data.sort(() => 0.5 - Math.random()).slice(0, 4 * 3);
         return grid.map((data, index) => {
@@ -33,6 +58,7 @@ function Cards() {
                 <div
                     key={index}
                     className="grid"
+                    onClick={() => handlePokemonClick(data.name)}
                 >
                     <img src={data.sprites.front_default} alt="" />
                     <p>{data.name}</p>
